ea451277f5a8ef21e2438a8e5bc34b79f0118b08
Yhonny Silva <yhonw21@gmail.com>
2024-03-10 21:53:33 -0300

    update

--- basketball_player.py
+++ basketball_player.py
@@ -54,20 +54,6 @@ def filedownload(df):
 
 st.markdown(filedownload(df_selected_team), unsafe_allow_html=True)
 
-# Heatmaps
-if st.button('Intercorrelation Heatmap'):
-    st.header('Intercorrelation Matrix Heatmap')
-    df_selected_team.to_csv('output.csv', index=False)
-    df = pd.read_csv('output.csv')
-    
-    corr = df.corr()
-    mask = np.zeros_like(corr)
-    mask[np.triu_indices_from(mask)] = True
-    with sns.axes_style("white"):
-        f, ax = plt.subplots(figsize=(7, 5))
-        ax = sns.heatmap(corr, mask=mask, vmax=1, square=True)
-    st.pyplot()
-
 
 
     
